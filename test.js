c14597abfcdca884b759546dd61b9ddcc2baba13
Avatar-shop <110927462+Azizaga2001@users.noreply.github.com>
2023-01-15 13:32:45 +0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,32 @@
+//1)
+//создайте класс user с конструктором
+class user {
+    constructor() {
+        
+    }
+}
+//2)
+//инициализируйте параметры из конструкторе
+class User {
+    constructor(name, id) {
+        this.name = name
+        this.id = id
+    }
+    talk() {
+        console.log('I can talk');
+    }
+}
+
+const user3 = new User()
+
+console.log(user3.talk())
+
+//3)
+//создайте 2 обьекта user1, user2 используя ранее созданный класс User
+const user1 = new User('Azizaga', '1')
+const user2 = new User('Ismail', '2')
+//4)
+//добавьте в класс user из второго теста метод talk
+//метод talk должен выводить в консоли 'I can talk' сообщение при вызове
+//создайте обьект user3 используя класс User
+//запустите метод talk ранее добавленный в класс User для обьекта user3
